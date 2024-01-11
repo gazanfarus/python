6219897288f0106fc203a826238f2031878fecef
Gazanfar <gazanfarus@gmail.com>
2024-01-10 17:47:36 -0800

    hangman v1

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,106 +1,60 @@
 import random
+import hangman_art
+import hangman_words
+import os
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+clear = lambda: os.system('cls')
 
 end_of_game = False
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 word_length = len(chosen_word)
 
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
 
+print(hangman_art.logo)
+
+#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
+wrong_choices = []
 for _ in range(word_length):
     display += "_"
 
 lives = 6
 while not end_of_game:
+#    clear()
     guess = input("Guess a letter: ").lower()
 
-    if guess in display:
+    if guess in display or guess in wrong_choices:
       print("\nYou've already chosen this letter")
 
     if guess in chosen_word and not guess in display:
       print("\nNice!")
-  
+
     #Check guessed letter
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
-    if guess in chosen_word:
+    if guess in chosen_word or guess in wrong_choices:
       pass
     else:
       print("\nOh no, try again...")
       lives -= 1
-    print(stages[lives])
+      wrong_choices += guess
+    print(hangman_art.stages[lives])
 
-    #Join all the elements in the list and turn it into a String.
-    print(f"{' '.join(display)}")
+    print(f"{' '.join(display)}\n")
+    print(f"Letters you've already chosen:\n{' '.join(wrong_choices)}\n")
 
     #Check if user has got all letters.
     if "_" not in display:
         end_of_game = True
-        print("You win.")
+        print("\nYou win.")
     elif lives == 0:
       end_of_game = True
-      print("You've lost")
-
+      print("\nYou've lost...")
