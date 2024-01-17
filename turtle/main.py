cffdd383fd5a710c22831979fb17c2783a6401ca
Gazanfar <gazanfarus@gmail.com>
2024-01-16 23:17:57 -0800

    turtle

---
+++ turtle/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+
+turtle_gaz = Turtle()
+turtle_gaz.shape("turtle")
+turtle_gaz.color("red")
+turtle_gaz.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
