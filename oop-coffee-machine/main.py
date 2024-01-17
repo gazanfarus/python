a8ebb1ffee1defbc42c190eb86aa0bbefb3a7403
Gazanfar <gazanfarus@gmail.com>
2024-01-16 18:44:49 -0800

    oop coffee machine  final

--- oop-coffee-machine/main.py
+++ oop-coffee-machine/main.py
@@ -3,8 +3,23 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
-menu_item = MenuItem()
 make_coffee = CoffeeMaker()
 money = MoneyMachine()
+is_on = True
 
-choice = input(f"Select your drink {menu.get_items()}")
+while is_on:
+    make_coffee.report()
+    money.report()
+    choice = input(f"Select your drink {menu.get_items()}: ").lower()
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        make_coffee.report()
+        money.report()
+    else:
+        drink = menu.find_drink(choice)
+        if drink != None:
+            if make_coffee.is_resource_sufficient(drink):
+                if money.make_payment(drink.cost):
+                    make_coffee.make_coffee(drink)
+
