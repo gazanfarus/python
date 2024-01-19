2aa33ba49a24173034f9e854182022bf808a8359
Gazanfar <gazanfarus@gmail.com>
2024-01-19 01:42:50 -0800

    turtle

--- turtle/turtle_race.py
+++ turtle/turtle_race.py
@@ -1,8 +1,39 @@
+import turtle
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
 screen = Screen()
-screen.setup(500, 400)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ").lower()
+colors = ["red", "orange", "brown", "green", "blue", "purple"]
+is_race_on = False
+all_turtles = []
+y_axis = -125
+
+for turtle_index in range(0, 6):
+    tim = Turtle("turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(-235, y_axis)
+    y_axis += 50
+    all_turtles.append(tim)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"Congratulation! The {winning_color} turtle won!")
+            else:
+                print(f"You've lost! The {winning_color} turtle won...")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
 
 
 screen.exitonclick()
