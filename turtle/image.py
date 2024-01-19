a01d9b52778460f883ec2e22ca5e9abeb948e639
Gazanfar <gazanfarus@gmail.com>
2024-01-18 23:53:31 -0800

    turtle

--- turtle/image.py
+++ turtle/image.py
@@ -1,11 +1,43 @@
-import colorgram
+# import colorgram
 import turtle
 from turtle import Turtle, Screen
 import random
 
-
 turtle.colormode(255)
+color_list = [
+    (213, 154, 96), (52, 107, 132), (179, 77, 31), (202, 142, 31), (115, 155, 171), (124, 79, 99), (122, 175, 156)
+]
 
-colors = colorgram.extract('image.jpg', 10)
-colors.rgb()
-print(colors)
+tim = Turtle()
+tim.shape("turtle")
+tim.speed(0)
+tim.penup()
+
+# colors = colorgram.extract('image.jpg', 10)
+#
+# rgb_colors = []
+# for color in colors:
+#    r = color.rgb.r
+#    g = color.rgb.g
+#    b = color.rgb.b
+#    new_color = (r, g, b)
+#    rgb_colors.append(new_color)
+# print(rgb_colors)
+
+def change_color():
+    random.choice(color_list)
+    return random.choice(color_list)
+
+
+y_axis = 30
+for _ in range(10):
+    for _ in range(10):
+        tim.dot(20, change_color())
+        tim.forward(30)
+    tim.teleport(0, y_axis)
+    y_axis += 30
+tim.hideturtle()
+
+
+screen = Screen()
+screen.exitonclick()
