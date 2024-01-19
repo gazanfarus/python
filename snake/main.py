299aa717ee18e7bd0396bcad9b24f7783ab886c4
Gazanfar <gazanfarus@gmail.com>
2024-01-19 11:15:52 -0800

    listen to keystrokes to move

--- snake/main.py
+++ snake/main.py
@@ -11,6 +11,12 @@ screen.tracer(0)
 screen.title("Python the Snake")
 snake = Snake()
 
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 # Snake movement
 game_is_on = True
 while game_is_on:
--- snake/snake.py
+++ snake/snake.py
@@ -1,17 +1,22 @@
 from turtle import Turtle
+MOVE_DISTANCE = 20
+
 
 class Snake:
     def __init__(self):
         self.snake = []
-        self.starting_x_position = 0
+        self.create_snake()
+        self.head = self.snake[0]
+
+    def create_snake(self):
+        starting_x_position = 0
         for _ in range(0, 3):
-            self.turtle = Turtle("square")
-            #    turtle = Turtle("turtle")
-            self.turtle.color("white")
-            self.turtle.penup()
-            self.turtle.goto(x=self.starting_x_position, y=0)
-            self.starting_x_position -= 20
-            self.snake.append(self.turtle)
+            turtle = Turtle("square")
+            turtle.color("white")
+            turtle.penup()
+            turtle.goto(x=starting_x_position, y=0)
+            starting_x_position -= 20
+            self.snake.append(turtle)
 
 # Snake movement
     def move(self):
@@ -19,4 +24,18 @@ class Snake:
             new_x = self.snake[segment_number - 1].xcor()
             new_y = self.snake[segment_number - 1].ycor()
             self.snake[segment_number].goto(new_x, new_y)
-        self.snake[0].forward(20)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.seth(90)
+
+    def down(self):
+        self.head.seth(270)
+
+    def left(self):
+        self.head.seth(180)
+
+    def right(self):
+        self.head.seth(0)
+
+
