87ccc06f41618856dbfac83b31828304f6ec3113
Gazanfar <gazanfarus@gmail.com>
2024-01-19 16:05:20 -0800

    food and scoreboard

--- snake/main.py
+++ snake/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from score import Scoreboard
 
 
 screen = Screen()
@@ -8,7 +10,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.tracer(0)
 screen.title("Python the Snake")
+
 snake = Snake()
+food = Food()
+score = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,5 +27,8 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    if snake.head.distance(food) < 15:
+        score.increase_score()
+        food.refresh()
 
 screen.exitonclick()
