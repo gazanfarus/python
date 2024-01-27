937cf794e4070ec8339cb49b0a9039d496a13820
Gazanfar <gazanfarus@gmail.com>
2024-01-27 13:46:30 -0800

    final code

--- mail_merge/main.py
+++ mail_merge/main.py
@@ -6,3 +6,13 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+with open("./Input/Names/invited_names.txt", mode="r") as names:
+    for name in names.readlines():
+        name = name.strip()
+        with open("./Input/Letters/starting_letter.txt", mode="r") as starting_letter:
+            letter = starting_letter.read()
+            new_letter = letter.replace("[name]", f"{name}")
+            with open(f"./Output/ReadyToSend/letter_to_{name}", mode="w") as final_letters:
+                final_letters.write(new_letter)
